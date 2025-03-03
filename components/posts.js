cd6c384405225ced1bfe908280e18f4b62d07c6a
spaiz <spaiz@lageo.com.sv>
2025-03-03 11:13:51 -0600

    mutation data

--- components/posts.js
+++ components/posts.js
@@ -1,7 +1,10 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+"use client";
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { tooglePostLikeStatus } from "@/actions/posts";
+import { useOptimistic } from "react";
 
-function Post({ post }) {
+function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
@@ -12,14 +15,19 @@ function Post({ post }) {
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form
+              action={action.bind(null, post.id)}
+              className={post.isLiked ? "liked" : ""}
+            >
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
@@ -29,15 +37,37 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevPost, updatedPostId) => {
+      const updatedPostIndex = prevPost.findIndex(
+        (post) => post.id === updatedPostId
+      );
+      if (updatedPostIndex === -1) {
+        return prevPost;
+      }
+      const updtaedPost = { ...prevPost[updatedPostIndex] };
+      updtaedPost.likes = updtaedPost.likes + (updtaedPost.isLiked ? -1 : 1);
+      updtaedPost.isLiked = !updtaedPost.isLiked;
+      const newPosts = [...prevPost];
+      newPosts[updatedPostIndex] = updtaedPost;
+      return newPosts;
+    }
+  );
+  if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
+  }
+
+  async function updatePost(postId) {
+    updateOptimisticPosts(postId);
+    await tooglePostLikeStatus(postId);
   }
 
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatePost} />
         </li>
       ))}
     </ul>
